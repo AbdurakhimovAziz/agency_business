dd6abef2abf3ba3520bfd17951479fa53bcdd196
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-01-24 17:01:16 +0500

    fix: fix scroll bar in burger menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav__list');
+const body = document.querySelector('body');
 
 burger.addEventListener('click', e => {
 	e.preventDefault();
 	burger.classList.toggle('burger-active');
 	nav.classList.toggle('active');
+	body.classList.toggle('disabled');
 });
