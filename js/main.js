a5d1e3ff277c72fda8eb5b0894f1aab88b773232
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-01-28 21:56:44 +0500

    fix: close menu on click in other sections

--- js/main.js
+++ js/main.js
@@ -4,9 +4,27 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav__list');
 const body = document.querySelector('body');
 
-burger.addEventListener('click', e => {
-	e.preventDefault();
+/* Toggle class to open/close nav menu, block/unblock body to control scrolling, transform burger icon */
+const toggleClass = function() {
 	burger.classList.toggle('burger-active');
 	nav.classList.toggle('active');
 	body.classList.toggle('disabled');
+}
+
+/* track clicking on burger */
+burger.addEventListener('click', e => {
+	e.stopPropagation();
+	toggleClass();
+});
+
+/* to hide menu when other sections are clicked */
+document.addEventListener('click', function(e) {
+    const target = e.target;
+    const isNav = target == nav || nav.contains(target);
+    const isBurger = target == burger;
+    const isActiveNav = nav.classList.contains('active');
+    
+    if (!isNav && !isBurger && isActiveNav) {
+        toggleClass();
+    }
 });
